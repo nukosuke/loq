185f4109a80f6e26744c1915d9b77bbf27e2ebf8
nukosuke <39.nuko@gmail.com>
2016-05-17 18:47:20 +0900

    add test code to check mocha

---
+++ test/server/controllers/user-controller.spec.js
@@ -0,0 +1,9 @@
+var expect     = require('chai').expect;
+var controller = require('../../../server/controllers/user-controller');
+
+describe('user controller', () => {
+  //TODO: remove
+  it('mocha check', done => {
+    done();
+  });
+});
