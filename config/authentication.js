f8e9ca5798719f6a9bdf57f3bc717bf28456a10b
nukosuke <39.nuko@gmail.com>
2016-05-20 00:26:35 +0900

    add oauth todo

--- config/authentication.js
+++ config/authentication.js
@@ -5,10 +5,32 @@
  * configuration
  */
 module.exports = {
-  'JWT': { //= Json Web Token (https://tools.ietf.org/html/rfc7519)
+  /**
+   * Json Web Token
+   * https://tools.ietf.org/html/rfc7519
+   */
+  JWT: {
     jwtFromRequest: require('passport-jwt').ExtractJwt.fromAuthHeader(),
     secretOrKey: 'secret',
     issuer: process.env.JWT_ISSUER,
     audience: process.env.JWT_AUDIENCE,
   },
+
+  /**
+   * OAuth providers
+   */
+   providers: {
+     twitter: {
+       //TODO:
+     },
+     facebook: {
+       //TODO:
+     },
+     /* and additional providers
+     github: {
+
+     },
+     */
+  }
+
 };
