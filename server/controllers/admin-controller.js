4b7da600f13e50abc372745eae0a1e68fe1a026d
nukosuke <39.nuko@gmail.com>
2016-05-20 05:41:46 +0900

    move instance creation into server/index

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -4,10 +4,8 @@ var BaseController = require('./base-controller');
 /**
  * AdminController
  */
-class AdminController extends BaseController {
+module.exports = class AdminController extends BaseController {
   index(req, res) {
     return res.render('admin/index', { title: 'admin' });
   }
 };
-
-module.exports = new AdminController();
