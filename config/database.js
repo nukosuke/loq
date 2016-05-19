2259e6ead39eb6f656b89b4707fa009e0a62717b
nukosuke <39.nuko@gmail.com>
2016-05-20 00:24:56 +0900

    シングルクォートに統一

--- config/database.js
+++ config/database.js
@@ -1,20 +1,20 @@
 module.exports = {
-  "development": {
-    "storage": "db/development.db",
-    "dialect": "sqlite",
+  development: {
+    storage: 'db/development.db',
+    dialect: 'sqlite',
   },
 
-  "test": {
-    "storage": "db/test.db",
-    "dialect": "sqlite",
+  test: {
+    storage: 'db/test.db',
+    dialect: 'sqlite',
   },
 
-  "production": {
-    "url": process.env.DATABASE_URL,
-    "pool": {
-      "max": 5,
-      "min": 0,
-      "idle": 100000,
+  production: {
+    url: process.env.DATABASE_URL,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 100000,
     },
   },
 };
