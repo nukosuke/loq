fadd2ad14f69ce07c38bb74181ed22bd13325e55
nukosuke <39.nuko@gmail.com>
2016-05-21 04:24:19 +0900

    delete needless methods

--- server/controllers/page-controller.js
+++ server/controllers/page-controller.js
@@ -5,18 +5,7 @@ var BaseController = require('./base-controller');
  * PageController
  */
 module.exports = class PageController extends BaseController {
-  show(res, req) {
-
-  }
-
-  show_html(res, req) {
-
-  }
-
-  //TODO: isomorphic markdown rendering engine
-  //it will be used for client markdown editor
-  //this method is for rendering README
-  show_markdown(res, req) {
-
+  show(req, res) {
+    res.render(`pages/${req.params.name}`, {});
   }
 };
