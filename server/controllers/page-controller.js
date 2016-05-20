c1e2d45e7b643a020bd353dbcc6d1b22fec34ff3
nukosuke <39.nuko@gmail.com>
2016-05-20 22:29:07 +0900

    add PageController

---
+++ server/controllers/page-controller.js
@@ -0,0 +1,22 @@
+'use strict';
+var BaseController = require('./base-controller');
+
+/**
+ * PageController
+ */
+module.exports = class PageController extends BaseController {
+  show(res, req) {
+
+  }
+
+  show_html(res, req) {
+
+  }
+
+  //TODO: isomorphic markdown rendering engine
+  //it will be used for client markdown editor
+  //this method is for rendering README
+  show_markdown(res, req) {
+
+  }
+};
