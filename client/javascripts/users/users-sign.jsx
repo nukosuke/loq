f6e94e320c976234b4e5a14abf1cd25a8e6b71f3
nukosuke <39.nuko@gmail.com>
2016-05-21 15:02:57 +0900

    add forms

--- client/javascripts/users/users-sign.jsx
+++ client/javascripts/users/users-sign.jsx
@@ -16,7 +16,19 @@ class UserSignApp extends React.Component {
   render() {
     return (
       <div>
-        <p>this is login page</p>
+        <form id='sign-in-form'>
+          <input type='text'     name='identifier' placeholder='ID or email' />
+          <input type='password' name='password'   placeholder='password' />
+          <button>LOGIN</button>
+        </form>
+
+        <form id='sign-up-form'>
+          <input type='text'     name='user[name]'             placeholder='ID' />
+          <input type='text'     name='user[email]'            placeholder='email address' />
+          <input type='password' name='user[password]'         placeholder='password' />
+          <input type='password' name='user[password_confirm]' placeholder='password again' />
+          <button>SIGNUP</button>
+        </form>
       </div>
     );
   }
