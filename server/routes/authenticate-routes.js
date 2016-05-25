3f0ee5f935ee4be71c04483032591dce4d6d67f9
nukosuke <39.nuko@gmail.com>
2016-05-26 03:54:00 +0900

    formatting

--- server/routes/authenticate-routes.js
+++ server/routes/authenticate-routes.js
@@ -6,17 +6,10 @@
 module.exports = function(controllers) { //TODO: (controllers, middlewares)
   var router = require('express').Router();
 
-  router.get('/authenticate(/*)?', controllers.user.authenticate);
-
-  //TODO: create AuthController
-  router.post('/authenticate/token', controllers.user.authenticateByPassword);
+  router.get   ('/authenticate(/*)?' , controllers.user.authenticate);
+  router.post  ('/authenticate/token', controllers.user.token);
   router.delete('/authenticate/token', (req, res) => res.json({message: 'not implemented'}));
 
-  router.get(
-    '/jwtTest',
-    controllers.user.authenticateByJWT,
-    controllers.user.needJwtRoute
-  );
   //TODO:
   //twitter & facebook & github strategy
   //var existProvider = function(req, res) { req.params.provider in providers ? }
