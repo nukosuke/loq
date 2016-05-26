b6adad82e62e79cab90c23845ba0085184aa108d
nukosuke <39.nuko@gmail.com>
2016-05-26 21:40:43 +0900

    move some methods into ApiUserController

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,7 +1,5 @@
 'use strict';
 var BaseController = require('./base-controller');
-var HttpStatus     = require('http-status');
-var jwt            = require('jsonwebtoken');
 
 /**
  * UserController
@@ -28,58 +26,6 @@ module.exports = class UserController extends BaseController {
 
   settings(req, res) {
     return res.render('users/settings', { title: 'user settings'});
-  }
-
-  /**
-   * API: authenticate and publish JWT
-   *TODO: move to api-user-controller.js
-   */
-  token(req, res, next) {
-    var config    = this.config;
-    var jwtConfig = config.authentication.JWT;
-    var passport  = this.middlewares.passport;
-
-    //FIXME: too much logic in controller!
-    passport.authenticate('local', function(err, user, info) {
-      if (err) {
-        return res.json(err);
-      }
-
-      if (!user) {
-        return res
-        .status(HttpStatus.UNAUTHORIZED)
-        .json({
-          status: HttpStatus.UNAUTHORIZED,
-          message: HttpStatus[HttpStatus.UNAUTHORIZED]
-        });
-      }
-
-      //TODO: remove unneccesary value from user and add JWT optionals in Strategy
-      jwt.sign(user.toJSON(), jwtConfig.secretOrKey, jwtConfig.options, function(err, token) {
-        if (err) {
-          return res
-          .status(HttpStatus.UNAUTHORIZED)
-          .json({
-            status: HttpStatus.UNAUTHORIZED,
-            message: HttpStatus[HttpStatus.UNAUTHORIZED]
-          });
-        }
-        return res.json({
-          status: HttpStatus.OK,
-          message: HttpStatus[HttpStatus.OK],
-          JWT: token,
-        });
-      });
-    })(req, res, next);
-  }
-
-  /**
-   * TODO: move into api-user-controller as index()
-   */
-  apiIndex(req, res) {
-    this.models.User.findAll().then(function(users) {
-      res.json({users});
-    });
   }
 
   /**
