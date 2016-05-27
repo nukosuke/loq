2f57deb01bc347931ae290934a6b6758049b9293
nukosuke <39.nuko@gmail.com>
2016-05-27 20:49:43 +0900

    add user profile set

---
+++ client/javascripts/users/components/UserProfile.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { userProfileReducer, userFollowReducer } from '../reducers/user-profile-reducer'
+import { fetchUserProfile } from '../actions/user-profile-actions'
+
+class UserProfile extends React.Component {
+  render() {
+    const { id, uid, name, family_name, given_name, bio, avatar, url } = this.props.profile;
+
+    return (
+      <div class='user-profile'>
+        <ul>
+          <li>{ avatar }</li>
+          <li>id: { id }</li>
+          <li>{ name }</li>
+          <li>@{ uid }</li>
+          <li>{ [family_name,given_name].join(' ') }</li>
+          <li>{ bio }</li>
+          <li><a class='url' href='{ url }' title='url'>{ url }</a></li>
+        </ul>
+        <button>follow</button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    const { uid }      = this.props.params;
+    dispatch(fetchUserProfile(uid));
+  }
+}
+export default connect(
+  state => ({
+    profile: state.userProfileReducer,
+    follow: state.userFollowReducer,
+  })
+)(UserProfile);
