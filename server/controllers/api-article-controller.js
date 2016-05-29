7f6305c96e63b5db0051b0a31d35d0b089fbbea6
nukosuke <39.nuko@gmail.com>
2016-05-29 16:56:09 +0900

    add show method to controller

--- server/controllers/api-article-controller.js
+++ server/controllers/api-article-controller.js
@@ -4,12 +4,26 @@ var BaseController = require('./base-controller');
 module.exports = class ApiArticleController extends BaseController {
   constructor(app) {
     super(app);
-    this.index = this.index.bind(this);
+    this.status = this.middlewares.httpStatus;
+    this.middlewares._.bindAll(this, 'index', 'show');
   }
 
   index(req, res) {
     this.models.Article
     .findAll()
     .then(articles => res.json({ articles }));
+  }
+
+  show(req, res) {
+    this.models.Article
+    .findOne({
+      where: { id: req.params.id },
+    })
+    .then(article => {
+      if (!article) {
+        return res.status(this.status.NOT_FOUND).json({});
+      }
+      res.json({ article });
+    });
   }
 }
--- server/controllers/api-user-controller.js
+++ server/controllers/api-user-controller.js
@@ -4,26 +4,26 @@ var BaseController = require('./base-controller');
 module.exports = class ApiUserController extends BaseController {
   constructor(app) {
     super(app);
-    this.middlewares._.bindAll(this, 'index', 'show', 'token')
+    this.status = this.middlewares.httpStatus;
+    this.middlewares._.bindAll(this, 'index', 'show', 'token');
   }
 
   index(req, res) {
-    this.models.User.findAll()
+    this.models.User
+    .findAll()
     .then(users => res.json({ users }));
   }
 
   show(req, res) {
     this.models.User
-    .findOne({ where: { uid: req.params.uid } })
+    .findOne({
+      where: { uid: req.params.uid },
+    })
     .then(user => {
       if(!user) {
-        return res
-        .status(this.middlewares.httpStatus.NOT_FOUND)
-        .json({ user: {} });
+        return res.status(this.status.NOT_FOUND).json({});
       }
-      return res
-      .status(this.middlewares.httpStatus.OK)
-      .json({ user });
+      res.json({ user });
     });
   }
 
