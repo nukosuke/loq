63174704dc91871d17f488e294ef9489b7f6435b
nukosuke <39.nuko@gmail.com>
2016-05-29 16:34:01 +0900

    fix model definition

--- server/models/article.js
+++ server/models/article.js
@@ -1,20 +1,78 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Article = sequelize.define('Article', {
-    user_id: DataTypes.NUMBER,
-    published: DataTypes.BOOL,
-    year: DataTypes.NUMBER,
-    month: DataTypes.NUMBER,
-    day: DataTypes.NUMBER,
-    slug: DataTypes.STRING,
-    title: DataTypes.STRING,
-    body: DataTypes.TEXT
+
+  /**
+   * make default slug from published_at in form of UNIX time
+   * if no slug param is given
+   */
+  var defaultSlugHook = function(article, options, callback) {
+    if (article.get('slug') === '' ) {
+      article.set('slug', (new Date(article.get('published_at')).getTime()));
+    }
+    callback(null, options);
+  };
+
+
+  var Article = sequelize.define('article', {
+    user_id: {
+      type: DataTypes.INTEGER,
+    },
+    published: {
+      type: DataTypes.BOOLEAN,
+    },
+    published_at: {
+      type: DataTypes.DATE,
+      validate: {
+        isDate: true,
+      },
+    },
+    slug: {
+      type: DataTypes.STRING,
+      validate: {
+        is: /^[a-zA-Z0-9-]/,
+        len: [1,32],
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [0,64],
+      },
+    },
+    body_markdown: {
+      type: DataTypes.TEXT,
+    }
   }, {
+    underscored: true,
+    defaultScope: {
+      where: { published: true },
+      attributes: ['id', 'published_at', 'slug', 'title', 'body_markdown'],
+    },
+    scopes: {
+      show: {
+        where: { published: true },
+        attributes: ['id', 'published_at', 'slug', 'title', 'body_markdown'],
+      }
+    },
+    hooks: {
+      beforeCreate: defaultSlugHook,
+      beforeUpdate: defaultSlugHook,
+    },
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Article.belongsTo(models.User, {
+          onDelete: 'CASCADE',
+        });
       }
-    }
+    },
+    instanceMethods: {
+      publish: function(callback) {
+        this.set('published', true);
+      },
+      unpublish: function(callback) {
+        this.set('published', false);
+      },
+    },
   });
   return Article;
 };
