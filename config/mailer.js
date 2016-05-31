9d77744da6bfd2949584e8188f3570cefab78cb4
nukosuke <39.nuko@gmail.com>
2016-06-01 00:14:14 +0900

    update mailer config

--- config/mailer.js
+++ config/mailer.js
@@ -5,14 +5,21 @@
  */
 module.exports = {
   development: {
-    pool: true,
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: { // your gmail account
-      user: '',
-      pass: '',
+    // https://github.com/nodemailer/nodemailer#set-up-smtp
+    smtp: {
+      pool: true,
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: { // your gmail account
+        user: '',
+        pass: '',
+      },
     },
+    // https://github.com/nodemailer/nodemailer#e-mail-message-fields
+    fields: {
+      from: 'noreply@example.com'
+    }
   },
 
   test: {
