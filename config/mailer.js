f90b4cbac87ccda1a3f257f7981a171e4eb5ce5f
nukosuke <39.nuko@gmail.com>
2016-06-01 22:58:25 +0900

    add pool config

--- config/mailer.js
+++ config/mailer.js
@@ -8,13 +8,17 @@ module.exports = {
     // https://github.com/nodemailer/nodemailer#set-up-smtp
     smtp: {
       pool: true,
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      //host: 'smtp.gmail.com',
+      //port: 465,
+      //secure: true,
+      service: 'gmail',
       auth: { // your gmail account
         user: '',
         pass: '',
       },
+      maxConnections: 5,
+      maxMessages: 10,
+      rateLimit: 5,
     },
     // https://github.com/nodemailer/nodemailer#e-mail-message-fields
     fields: {
