62680057920471f523d4e4e124f13f1d1d885696
nukosuke <39.nuko@gmail.com>
2016-06-02 22:02:53 +0900

    close #14, NODE_ENV=production gulp build

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV || 'development'}"`,
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
   ]
 };
