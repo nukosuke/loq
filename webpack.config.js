208bb76100fb7e379ca46c5ca391aa1f3bf9f886
nukosuke <39.nuko@gmail.com>
2016-06-02 22:13:30 +0900

    disable UglifyJsPlugin warnings

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': `"${process.env.NODE_ENV || 'development'}"`,
     }),
-    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
   ]
 };
