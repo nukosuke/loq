f2ed0e76c693f313f79fcefc79ba7ff062a2b880
nukosuke <39.nuko@gmail.com>
2016-06-02 18:51:59 +0900

    inject constants

--- server/middlewares/middleware.js
+++ server/middlewares/middleware.js
@@ -6,8 +6,9 @@
 module.exports = class Middleware {
   constructor(app) {
     if (app) {
-      this.config = app.get('config') || {};
-      this.models = app.get('models') || {};
+      this.config    = app.get('config')    || {};
+      this.constants = app.get('constants') || {};
+      this.models    = app.get('models')    || {};
     } else {
       this.config = this.models = {};
     }
