247349fe46801aaf9e0146398bfe46820e85cca2
nukosuke <39.nuko@gmail.com>
2016-06-05 18:00:23 +0900

    add uppercase option

--- server/utils/dir-auto-loader.js
+++ server/utils/dir-auto-loader.js
@@ -4,15 +4,16 @@
  */
 'use strict';
 
-module.exports = function(dirname) {
-  var modules = {};
+module.exports = function(dirname, uppercase) {
+  var modules   = {};
+  uppercase = uppercase || true;
 
   require('fs').readdirSync(dirname + '/').forEach(file => {
     if (file.match(/\.js$/) !== null && file !== 'index.js') {
       var name = file
       .replace('.js', '')
       .split('-')
-      .map(term => term[0].toUpperCase() + term.slice(1))
+      .map(term => uppercase ? term[0].toUpperCase() + term.slice(1) : term)
       .join('');
 
       modules[name] = require(dirname + '/' + file);
