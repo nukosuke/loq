aa0d7854676f934901a709489ebb8340a77ddf0a
nukosuke <39.nuko@gmail.com>
2016-06-06 03:46:39 +0900

    add servers var

--- server/controllers/article-controller.js
+++ server/controllers/article-controller.js
@@ -4,13 +4,22 @@ var BaseController = require('./base-controller');
 module.exports = class ArticleController extends BaseController {
   constructor(app) {
     super(app);
+    this._ = this.middlewares._;
+    this._.bindAll(this, 'index', 'show');
   }
 
   index(req, res) {
-    res.render('articles/index', { title: 'articles' });
+    res.render('articles/index', {
+      title: 'articles',
+      servers: this.config.config.servers,
+    });
   }
 
   show(req, res) {
-    res.render('articles/show', { title: 'articles show' });
+
+    res.render('articles/show', {
+      title: 'articles show',
+      servers: this.config.config.servers,
+    });
   }
 };
